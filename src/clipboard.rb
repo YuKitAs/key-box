876351588ec1acc97d626f51b1e3ebf44b5648a6
xiGUAwanOU <xiguawanou@gmail.com>
2018-03-19 09:45:45 +0100

    Added macOS support.

--- src/clipboard.rb
+++ src/clipboard.rb
@@ -4,8 +4,8 @@ class Clipboard
   def self.copy(content)
     if !(/linux/ =~ RUBY_PLATFORM).nil?
       Open3.popen3("xclip -selection clipboard"){|input, _, _| input << content}
-    elsif !(/mac/ =~ RUBY_PLATFORM).nil?
-      puts(`echo #{content}`)
+    elsif !(/darwin/ =~ RUBY_PLATFORM).nil?
+      `echo '#{content}' | pbcopy`
     end
   end
 end
