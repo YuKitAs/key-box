8bd789d35fc22126394a13ecf30bcd658fbc57d7
YuKitAs <moncher.merci@gmail.com>
2018-06-09 01:43:17 +0200

    Tried to build a release version.

--- desktop/main.js
+++ desktop/main.js
@@ -4,8 +4,17 @@ const url = require('url')
 
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.loadURL('http://localhost:8080')
-  mainWindow.webContents.openDevTools()
+
+  if (process.argv[2] === 'dev') {
+    mainWindow.loadURL('http://localhost:8080')
+    mainWindow.webContents.openDevTools()
+  } else {
+    mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'dist', 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+  }
 }
 
 app.on('ready', createWindow)
